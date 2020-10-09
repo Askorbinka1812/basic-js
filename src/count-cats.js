4d6172006b79bd4d3f0de5afeef6d7fe6135077a
Askorbinka1812 <65978553+Askorbinka1812@users.noreply.github.com>
2020-10-09 22:01:40 +0300

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let sum = 0;
+  for (let arr of matrix){
+    for(let item of arr){
+    if(item === "^^"){
+      sum += 1;
+      }
+    }
+  }
+  return sum;
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, depth = 1) {
+    let underArray = arr.filter(a => Array.isArray(a));
+    return underArray.length ? this.calculateDepth(underArray.flat(), depth + 1) : depth;
   }
 };
